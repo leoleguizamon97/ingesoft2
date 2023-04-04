d4e98043c4dd11ac5519d15c9cdc2e39e71ab207
leoleguizamon <anlleguizamongu@unal.edu.co>
2023-04-04 13:21:53 -0500

    format 2.0

--- taller0/web/app.js
+++ taller0/web/app.js
@@ -1,2 +1,15 @@
+//Express 
 const express = require('express');
 const app = express();
+
+/*app.get('/',(req,res) => {
+	res.send('ola de mar');
+})*/
+
+//Motor de plantillas
+app.set('view engine','ejs');
+
+app.listen(8000, ()=>{
+	console.log('server rruning en http://localhost:8000');
+});
+
