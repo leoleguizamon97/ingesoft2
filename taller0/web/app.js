79a1b311b69c1bba613d7f2acbee38cf84bcdca8
leoleguizamon <anlleguizamongu@unal.edu.co>
2023-04-05 02:33:34 -0500

    test

--- taller0/web/app.js
+++ taller0/web/app.js
@@ -1,14 +1,13 @@
 //Express 
 const express = require('express');
+const favicon = require('serve-favicon');
 const app = express();
 
-app.use('/favicon.ico', express.static('../logo.svg'));
 //Motor de plantillas
 app.set('view engine','ejs');
-
+app.use(express.static(__dirname + '/imagenes'));
 //Usa el router
 app.use('/', require('./router'));
-
 app.listen(8000, ()=>{
 	console.log('server rruning en http://localhost:8000');
 });
